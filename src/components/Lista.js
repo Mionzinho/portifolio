6ae9b6644d0fea3dc3acab11d8ad1888cb43c952
unknown <vinicius.marsola2@gmail.com>
2019-08-05 16:26:31 -0300

    init

---
+++ src/components/Lista.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const c = () => {
+    return (
+      <>
+        <ul>
+            <li>1</li>
+            <li>1</li>
+                
+        </ul>
+      </>
+      )
+  }
+  
+  export default c
+  
---
+++ src/pages/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Lista from '../components/Lista'
+
+// import logo from './logo.svg';
+//<img src={logo} className="App-logo" alt="logo" />
+// 
+
+const App = () => {
+  return (
+    <>
+      <h1>qualquer coisa</h1>
+      <Lista></Lista>
+      <h2 className="titulo">dsdas</h2>
+    </>
+    )
+}
+
+export default App
