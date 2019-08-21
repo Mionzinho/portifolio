d87b4e47469bc6fe6f6c4ed4cecc78f956271752
Eric Havel <eric.havel@gmail.com>
2019-08-21 18:29:30 -0300

    rename projetos

--- src/pages/Projetos.js
+++ src/pages/Projetos.js
@@ -2,10 +2,6 @@ import React from 'react'
 import { connect } from 'react-redux'
 import Menu from '../components/Menu'
 
-// import logo from './logo.svg';
-//<img src={logo} className="App-logo" alt="logo" />
-// 
-
 const App = ({ projetos }) => {
   return ( 
     <>    
